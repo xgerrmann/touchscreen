3ac2549cdd5d9bd600a3e534d00cec05b83dd810
xgerrmann <xander@gerrmann.nl>
2017-02-10 19:34:58 +0100

    Place TouchManager in separate Library.

---
+++ lib/TouchManager/TouchManager.h
@@ -0,0 +1,61 @@
+#ifndef _TOUCHMANAGER_
+#define _TOUCHMANAGER_
+
+#include <Adafruit_GFX.h> // Core graphics library
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
+#include <MatrixMath.h>  // Library for matrix calculations
+#include <UTouch.h>
+
+#define EVER true
+
+struct touchAction
+{
+	float location[4]; // x, y, dx/dt, dy/dt (Location and speed for x and y)
+	// Type is one of:
+	// 1: Touch
+	// 2: ...
+	// 3: ...
+	int type;
+};
+
+class trackFilter{
+	private:
+		// Variables
+		float	A[16];
+		float	A_t[16];
+		float	R[4];
+		float	C[8];
+		float	Q[16];
+		float	P[16];
+		//int width_max, height_max;
+		float	dt;
+		float loc[4]; // [x,y,dx/dt,dy/dt]
+		float Pscale = 100;
+		float Qscale = 0.001;
+
+		// Procedures
+		void setConstantMatrices();
+		void setP();
+		void setLoc(float location[4]);
+	public:
+		// Variables
+
+		// Procedures
+		trackFilter(int width, int height, float d_t);
+		void reset(float measurement[2]);
+		void setMatrices();
+		void update(float measurement[2], float location[2]);
+};
+
+class touchManager
+{
+	private:
+		UTouch* tScreen;
+		trackFilter* filter;
+		int max_interval_ms = 100;	//  [ms] Max interval between consecutive data points belonging to the same touch action
+		int dt				= 7;	// [ms]
+	public:
+		touchManager( UTouch* tScreen);
+		touchAction getAction( void );
+};
+#endif
--- src/track_Kalman/touch.cpp
+++ src/track_Kalman/touch.cpp
@@ -4,117 +4,28 @@
 
 #include <Adafruit_GFX.h> // Core graphics library
 #include <Adafruit_TFTLCD.h> // Hardware-specific library
+#include <TouchManager.h> // Library for filtering touchscreen data
 #include <MatrixMath.h>  // Library for matrix calculations
 #include <UTouch.h>
 #include "definitions.h"
 
-
-float	dt = 7.0/1000.0; // 7 [ms]
-int		n_miss = 10;
-
-struct touchAction
-{
-	float location[4]; // x, y, dx/dt, dy/dt (Location and speed for x and y)
-	// Type is one of:
-	// 1: Touch
-	// 2: ...
-	// 3: ...
-	int type;
-};
-
-class touchManager
-{
-	private:
-		UTouch* tScreen;
-		trackFilter* filter;
-	public:
-		touchManager( UTouch* tScreen );
-		touchAction getAction( void );
-};
-
-touchManager::touchManager( UTouch* Tscreen )
-{
-	// Attach screen
-	this->tScreen = Tscreen;
-	
-	// Initialize filter
-	this->filter	= new trackFilter(420,380,dt);
-}
-
-touchAction touchManager::getAction()
-{
-	int x_meas, y_meas;
-	float x_filt, y_filt;
-	float measurement[2]= {};
-	float location[2]	= {};
-	int t_elapsed		= 0;	// [ms]
-	int t_start_touch	= 0;	// [ms]
-	int t_start_loop	= 0;
-	int t_end_loop		= 0;
-	bool filter_init	= false;
-	t_start_touch = millis(); // Timer for the duration of the touch action
-	for(EVER;;)
-	{
-		if(!this->tScreen->dataAvailable())
-		{
-			if((millis()-t_end_loop)>n_miss*dt*1000)
-			{
-				break; // Break while loop, touch action is interrupted
-			}
-			continue; // Restart while loop
-		}
-		t_start_loop = millis();
-	
-		// Reset the filter for each NEW touch action
-		if(!filter_init)
-		{
-			// Reset filter for current touch action
-			filter->reset(measurement);
-			filter_init = true;
-		}
-		tScreen->read();
-		y_meas = tScreen->getX();
-		x_meas = tScreen->getY();
-		if ((x_meas!=-1) and (y_meas!=-1))
-		{
-			y_meas=480-y_meas;
-			
-			measurement[0] = (float) x_meas;
-			measurement[1] = (float) y_meas;
-			filter->update(measurement,location);
-			x_filt	= (float) location[0];
-			y_filt	= (float) location[1];
-		}
-		t_end_loop	= millis();
-		t_elapsed	= t_end_loop-t_start_loop;
-		// Check if data is available for the next loop
-	}
-	touchAction tAction;
-	tAction.location[0] = x_filt;
-	tAction.location[1] = y_filt;
-	tAction.type	= 1;
-	return tAction;
-}
-
-touchManager* tManager;
-
 void setup(void)
 {
 	Serial.begin(9600);
-	
+
 	tft.reset();
-	
+
 	myTouch.InitTouch();
 	myTouch.setPrecision(PREC_LOW);
-	
+
 	tft.begin(0x9488);
 	tft.fillScreen(BLACK);
-	
+
 	tft.setCursor(280, 10);
 	tft.setTextColor(WHITE);
 	tft.setTextSize(3);
-	
-	tManager	= new touchManager(&myTouch);
+
+	tManager		= new touchManager(&myTouch);
 }
 
 void loop()
